96acd1a1c73fd05979fe285d77afcae7bed667b2
Likhithch123 <likhithgupthacholleti@gmail.com>
2024-01-26 11:37:35 +0530

    Updated Project_2.py

--- project_2.py
+++ project_2.py
@@ -2,9 +2,13 @@
 
 print("Welcome to the tip calculator")
 total_bill=input("What was the total bill?\n₹")
-percentage_tip=input("What percentage tip would you like to give (10, 12, 15) ?\n")
+percentage_tip=input("What percentage tip would you like to give (0, 10, 12, 15) ?\n")
 total_num_of_people=input("How many people to split the bill?\n")
 total_tip=(int(percentage_tip)/100)*float(total_bill)
 total_billwithtip=float(total_bill)+total_tip
 tip_individual=total_billwithtip/int(total_num_of_people)
-print(f"Each person should pay: ₹{round(tip_individual,2)}")
+final_amount="{:.2f}".format(tip_individual)
+print(f"Each person should pay: ₹{final_amount}")
+
+## if round function doesn't work properly then use the following code
+# "{:.2f}".format(tip_individual)
