b9ece9e531b1e5efb860318269f23ac73d93979f
Likhithch123 <likhithgupthacholleti@gmail.com>
2024-04-13 08:26:48 +0530

    DAY-26: Modified Nato Phonetic Alphabet Technique

--- PROJECT-26/main.py
+++ PROJECT-26/main.py
@@ -1,8 +1,16 @@
 import pandas
-data_dict = pandas.read_csv("nato_phonetic_alphabet.csv")
+data_dict = pandas.read_csv("../python-projects/PROJECT-26/nato_phonetic_alphabet.csv")
 # print(type(data_dict))
 new_dict = {row.letter: row.code for (index, row) in data_dict.iterrows()}
 # print(new_dict)
-user_input = input("Enter your name:").upper()
-nato_list = [new_dict[letter] for letter in user_input]
-print(nato_list)
+
+def generate_phonetic():
+    user_input = input("Enter your name:").upper()
+    try:
+        nato_list = [new_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(nato_list)
+generate_phonetic()
